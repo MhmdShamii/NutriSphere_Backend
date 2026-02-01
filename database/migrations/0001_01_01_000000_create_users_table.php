82fc4cc14b05f2d3212107721fe61b16ced0e94e
MhmdShamii <mhmddshamii@gmail.com>
2026-02-01 04:44:29 +0200

    make countries migration and link it to user

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string("last_name");
             $table->string('email')->unique();
             $table->string("phone");
-            $table->string("country");
+            $table->string("country_id")->references("id")->on("countries")->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
