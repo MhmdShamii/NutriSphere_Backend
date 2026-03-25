3a59277cdcb18cc3cf39a98455e7042d6f333bef
MhmdShamii <mhmddshamii@gmail.com>
2026-03-25 15:02:38 +0200

    change mail design

---
+++ resources/views/vendor/mail/html/footer.blade.php
@@ -0,0 +1,19 @@
+<tr>
+<td>
+<table class="footer" align="center" width="570">
+<tr>
+<td class="content-cell" align="center">
+
+<p style="color:#64748b;">
+© {{ date('Y') }} {{ config('app.name') }}
+</p>
+
+<p style="color:#475569; font-size:12px;">
+Track your nutrition. Improve your life.
+</p>
+
+</td>
+</tr>
+</table>
+</td>
+</tr>
---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,13 @@
+@props(['url'])
+<tr>
+<td class="header" style="padding: 30px 0;">
+    <a href="{{ $url }}" style="text-decoration: none;">
+        <h2 style="color:#22c55e; font-weight:bold;">
+            {{ config('app.name') }}
+        </h2>
+        <p style="color:#94a3b8; font-size:12px;">
+            Your AI Fitness & Nutrition Companion
+        </p>
+    </a>
+</td>
+</tr>
